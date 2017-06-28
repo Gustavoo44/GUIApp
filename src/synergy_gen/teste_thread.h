d57deffe9f5deb405ca2721744db302e4efc2f14
Gustavoo44 <gustavo_oliveira44@hotmail.com>
2017-06-28 14:38:15 -0300

    Inicio do Projeto

---
+++ src/synergy_gen/teste_thread.h
@@ -0,0 +1,19 @@
+/* generated thread header file - do not edit */
+#ifndef TESTE_THREAD_H_
+#define TESTE_THREAD_H_
+#include "bsp_api.h"
+#include "tx_api.h"
+#include "hal_data.h"
+#ifdef __cplusplus 
+extern "C" void teste_thread_entry(void);
+#else 
+extern void teste_thread_entry(void);
+#endif
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* TESTE_THREAD_H_ */
---
+++ src/teste_thread_entry.c
@@ -0,0 +1,12 @@
+#include "teste_thread.h"
+
+/* Teste_Thread entry function */
+void teste_thread_entry(void)
+{
+    /* TODO: add your own code here */
+    while (1)
+    {
+        toggle_led(0);
+        tx_thread_sleep (1);
+    }
+}
